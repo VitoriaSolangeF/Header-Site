335f008669765d67ce12fe7e3f649e71ff600d1d
unknown <vitory150518@gmail.com>
2023-09-13 16:12:51 -0300

    Front Finalizado

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import './Button.css';
+import { MdOutlineWhatsapp } from 'react-icons/md';
+import { BsCart3 } from 'react-icons/bs';
+import { RiContactsLine } from 'react-icons/ri';
+import { HiOutlineSpeakerphone } from 'react-icons/hi';
 
 function Button () {
     return (
         <div className="button">
             <button className="button-car" >
-                <img src="../promocao.png" alt="carrinho"/>
+                <div> <MdOutlineWhatsapp/> </div>
             </button>
 
             <button className="button-atendimento" >
-                <img src="../carrinho.png" alt="carrinho"/>
+                <div> <BsCart3/> </div>
             </button>
 
             <button className="button-promocao" >
-                <img src="../atendimento.png" alt="carrinho"/>
+                <div> <RiContactsLine/> </div>
             </button>
 
             <button className="button-zap" >
-                <img src="../zap.png" alt="carrinho"/>
+                <div> <HiOutlineSpeakerphone/> </div>
             </button>
         </div>
     );
