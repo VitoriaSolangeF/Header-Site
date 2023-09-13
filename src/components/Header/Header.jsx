97e325c42a08251b551a9f950c31d49591acc65d
unknown <vitory150518@gmail.com>
2023-09-13 13:28:45 -0300

    Search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React, { useState} from "react";
 import './Header.css';
 import Menu from "../Menu/Menu";
+import Search from "../Search/Search";
 
 function Header () {
     return (
         <header className="header">
             <div className="container-menu">
                 <img className="logo" src="logobranca.png" alt="logo"/>
+                
             </div>
             <div className="container-button">
                 <Menu />
+                <Search />
             </div>
         </header>
     );
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Search.css';
+
+function SearchBar () {
+    return (
+        <div className="input-wrapper">
+        
+        <input placeholder="Pesquisar Produtto..." />
+        </div>
+    )
+     
+}
+
+export default SearchBar;
